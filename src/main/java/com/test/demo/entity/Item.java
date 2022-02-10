845c59703d2892d536277ecbf849d3117c9a62dd
manjeet <manjeetm@dewsolutions.in>
2022-02-10 12:55:26 +0530

    Initial commit

---
+++ src/main/java/com/test/demo/entity/Item.java
@@ -0,0 +1,17 @@
+package com.test.demo.entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class Item {
+    private Integer quantity;
+    private String description;
+}
